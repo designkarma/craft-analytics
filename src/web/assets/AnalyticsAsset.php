28bcc007ba4f42e665bdc679c1675a8f8c462ce3
designkarma <info@designkarma.co.uk>
2026-09-21 11:02:04 +0100

    Build initial analytics dashboard widget

---
+++ src/web/assets/AnalyticsAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace designkarma\analytics\web\assets;
+
+use craft\web\AssetBundle;
+
+class AnalyticsAsset extends AssetBundle
+{
+    public function init(): void
+    {
+        $this->sourcePath = __DIR__ . '/dist';
+
+        $this->css = [
+            'css/analytics.css',
+        ];
+
+        parent::init();
+    }
+}
--- src/widgets/AnalyticsWidget.php
+++ src/widgets/AnalyticsWidget.php
@@ -2,7 +2,9 @@
 
 namespace designkarma\analytics\widgets;
 
+use Craft;
 use craft\base\Widget;
+use designkarma\analytics\web\assets\AnalyticsAsset;
 
 class AnalyticsWidget extends Widget
 {
@@ -17,7 +19,39 @@ class AnalyticsWidget extends Widget
     }
 
     public function getBodyHtml(): ?string
-    {
-        return '<p>Hello from Google Analytics 👋</p>';
-    }
+	{
+	    Craft::$app->getView()->registerAssetBundle(AnalyticsAsset::class);
+
+	    return Craft::$app->getView()->renderTemplate(
+	        'analytics/widgets/analytics',
+	        [
+	            'users' => 1248,
+	            'sessions' => 1683,
+	            'pageViews' => 2941,
+	            'topPages' => [
+	                [
+	                    'title' => 'Homepage',
+	                    'views' => 842,
+	                ],
+	                [
+	                    'title' => 'Craft CMS Development',
+	                    'views' => 463,
+	                ],
+	                [
+	                    'title' => 'Craft CMS vs WordPress',
+	                    'views' => 327,
+	                ],
+	                [
+	                    'title' => 'Contact',
+	                    'views' => 184,
+	                ],
+	            ],
+	            'dailyViews' => [
+				    82, 91, 88, 105, 112, 98, 121, 136, 129, 143,
+				    151, 147, 162, 155, 171, 184, 176, 193, 201, 188,
+				    214, 207, 221, 235, 228, 246, 239, 258, 271, 263,
+				],
+	        ]
+	    );
+	}
 }
